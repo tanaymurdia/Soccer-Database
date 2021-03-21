80a15dbd5b120692d723a60e56760769f799b047
Tanay Murdia <murdiat@dickinson.edu>
2021-03-21 23:03:51 +0000

    Added tables to Match Home and Player Home

--- server.py
+++ server.py
@@ -153,7 +153,73 @@ def index():
   #
   return render_template("index.html", **context)
 
+
+@app.route('/playerhome')
+def playerhome():
+  """
+  """
+  print(request.args)
+  cursor = g.conn.execute("select p.name, sum (score)as goals From matchstat m,player p where m.playerid=p.playerid Group by p.name order by goals desc;")
+  names = []
+  goals =[]
+  for result in cursor:
+      names.append(result[0])
+      goals.append(result[1])# can also be accessed using result[0]
+  cursor.close()
+  cursor = g.conn.execute(";select p.name,sum(m.assist) as total from matchstat m, player p where m.playerid=p.playerid group by p.name order by total desc;")
+  names1 = []
+  assists =[]
+  for result in cursor:
+      names1.append(result[0])
+      assists.append(result[1])# can also be accessed using result[0]
+  cursor.close()
+  cursor = g.conn.execute(";select p.name,sum(m.save) as total from matchstat m, player p where m.playerid=p.playerid group by p.name order by total desc;")
+  names2 = []
+  saves =[]
+  for result in cursor:
+      names2.append(result[0])
+      saves.append(result[1])# can also be accessed using result[0]
+  cursor.close()
+
+
+
+  context = dict(data = names,data1=goals,data2=names1,data3=assists,data4=names2,data5=saves)
+  return render_template("playerhome.html", **context)
+
+@app.route('/matchhome')
+def matchhome():
+  """
+  """
+  print(request.args)
+  cursor=g.conn.execute("select t.homeground, t1.homeground,m1.playdate from (select p.teamid as c1, p1.teamid as c2, p.matchid as c3 from playbetween p, playbetween p1 where p.matchid=p1.matchid and p.teamid<p1.teamid) m, team t, team t1,match m1 where m.c1=t.teamid and m.c2=t1.teamid and m.c3=m1.matchid and m1.playdate> date(now())")
+  team4 = []
+  team5 =[]
+  date1=[]
+  for result in cursor:
+      team4.append(result[0])
+      team5.append(result[1])
+      date1.append(result[2])# can also be accessed using result[0]
+  cursor.close()
+
+  #cursor = g.conn.execute("select t.homeground, t1.homeground,m1.playdate from (select p.teamid as c1, p1.teamid as c2, p.matchid as c3 from playbetween p, playbetween p1 where p.matchid=p1.matchid and p.teamid<p1.teamid) m, team t, team t1,match m1 where m.c1=t.teamid and m.c2=t1.teamid and m.c3=m1.matchid;")
+  cursor=g.conn.execute("select t.homeground, t1.homeground,m1.playdate,m.c4,m.c5 from (select p.teamid as c1, p1.teamid as c2, p.matchid as c3, p.teamscore as c4, p1.teamscore as c5 from playbetween p, playbetween p1 where p.matchid=p1.matchid and p.teamid<p1.teamid) m, team t, team t1,match m1 where m.c1=t.teamid and m.c2=t1.teamid and m.c3=m1.matchid and m1.playdate<=date(now());")
+  team1 = []
+  team2 =[]
+  date=[]
+  score=[]
+  for result in cursor:
+      team1.append(result[0])
+      team2.append(result[1])
+      date.append(result[2])# can also be accessed using result[0]
+      score.append(str(result[3])+" - "+str(result[4]))
+  cursor.close()
+
+  context = dict(data = team1,data1=team2,data2=date, data3=score, data4=team4, data5=team5, data6=date1)
+  return render_template("matchhome.html", **context)
+
+
 @app.route('/player')
+
 def player():
   """
   """
